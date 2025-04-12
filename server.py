e89c966e0e2273157be2914697700284e6c7f395
Nicki tst <kanieleoutiz@gmail.com>
2025-04-12 09:36:34 +0300

    add orm_context()

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 from aiohttp import web
 
+from models import init_orm, close_orm
+
 app = web.Application()
+
+async def orm_context(app: web.Application):
+    print("start")
+    await init_orm()
+    yield
+    await close_orm()
+    print("finish")
 
 async def hello(request: web.Request):
     response = web.json_response({"hello": "world"})
     return response
+
+app.cleanup_ctx.append(orm_context)
 
 app.add_routes([
     web.post('/hello/world', hello)
