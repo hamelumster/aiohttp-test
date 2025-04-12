c29db7710e9f68d9b54204bde68697719be6c9fa
Nicki tst <kanieleoutiz@gmail.com>
2025-04-12 09:35:59 +0300

    add models.py

---
+++ models.py
@@ -0,0 +1,51 @@
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+from sqlalchemy import DateTime, func, ForeignKey, Integer, String
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5431")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "secret")
+DB_NAME = os.getenv("DB_NAME", "test_aiohttp")
+
+PG_DSN = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(repr(PG_DSN))
+
+engine = create_async_engine(PG_DSN)
+Session = async_sessionmaker(engine, expire_on_commit=False)
+
+class Base(DeclarativeBase, AsyncAttrs):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] = mapped_column(nullable=False)
+
+    announcements = relationship("Announcement", back_populates="owner_relationship")
+
+class Announcement(Base):
+    __tablename__ = "announcements"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    owner: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    owner_relationship = relationship("User", back_populates="announcements")
+
+async def init_orm():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def close_orm():
+    await engine.dispose()
